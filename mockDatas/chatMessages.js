f10b573f55290a6354e5ac50553bc4a7e7f70076
Nilesh <nkil123>
2023-08-14 04:42:15 +0530

    added mock data

---
+++ mockDatas/chatMessages.js
@@ -0,0 +1,74 @@
+export const chatMessages = [
+  {
+    content: "😞",
+    user: {
+      name: "nilesh",
+      id: 1
+    },
+    createdAt: "2023-08-14T21:40:00"
+  },
+  {
+    content: "Ohh!",
+    user: {
+      name: "nilesh",
+      id: 1
+    },
+    createdAt: "2023-08-14T21:40:00"
+  },
+  {
+    content: "What Sorry 🤬",
+    user: {
+      name: "nilesh",
+      id: 4
+    },
+    createdAt: "2023-08-14T21:40:00"
+  },
+  {
+    content: "Yeah I know Sorry :(",
+    user: {
+      name: "nilesh",
+      id: 1
+    },
+    createdAt: "2023-08-14T21:40:00"
+  },
+  {
+    content: "🤨😞",
+    user: {
+      name: "nilesh",
+      id: 4
+    },
+    createdAt: "2023-08-14T21:40:00"
+  },
+  {
+    content: "The meeting was suppose to end on time but.",
+    user: {
+      name: "nilesh",
+      id: 4
+    },
+    createdAt: "2023-08-14T21:40:00"
+  },
+  {
+    content: `Did you talk to Peter about yesterday? The meeting was not good and Shreya saying something in between.`,
+    user: {
+      name: "nilesh",
+      id: 4
+    },
+    createdAt: "2023-08-14T21:40:00"
+  },
+  {
+    content: "But I dont know about timing :(",
+    user: {
+      name: "nilesh",
+      id: 1
+    },
+    createdAt: "2023-08-14T21:40:00"
+  },
+  {
+    content: "Sure! Yes",
+    user: {
+      name: "nilesh",
+      id: 1
+    },
+    createdAt: "2023-08-14T21:30:00"
+  }
+];
